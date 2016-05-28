73511e65094ab6d494115c1cb275d0996c5b3e01
krikaz <krikaz@krikazs-macbook-pro.local>
2016-05-28 21:48:33 +0700

    Initial Commit

---
+++ yahtzee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yahtzee
+//
+//  Created by krikaz on 5/28/16.
+//  Copyright © 2016 krikaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
